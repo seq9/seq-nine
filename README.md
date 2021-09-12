fae7c82f49d2668fcdadb02aceccd9a76c1ec8db
arsize <arsizeof3416@gmail.com>
2021-09-12 23:26:53 +0800

    feat:init react vite

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # seqNine
-seq9 website build by vue3+typescript （主站前端项目）
+seq9 website build by react+vite+typescript （主站前端项目）
+
+## 技术选型
+
+1.react <br />
+2.vite <br />
+3.ts <br />
+4.style-components <br />
---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React, { useState } from 'react'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="App">
+      hello hooks
+    </div>
+  )
+}
+
+export default App
